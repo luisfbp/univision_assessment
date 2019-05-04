ac9015562e6fea6ad70a00ce8af9672b84419dda
LFBP <felipebejaranop@gmail.com>
2019-05-04 10:01:42 -0500

    Initial Commits

---
+++ src/main/java/com/univision/univisionTest/UnivisionTestApplication.java
@@ -0,0 +1,13 @@
+package com.univision.univisionTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivisionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivisionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univision/univisionTest/UnivisionTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.univision.univisionTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnivisionTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
